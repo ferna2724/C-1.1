4e0528909543940153cdf64c86b220c4bbe7b355
Fernando <Fernando@LAPTOP-GBJENCNE>
2023-04-05 14:53:15 -0400

    Add project files.

---
+++ C# 1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace C__1._1;
+class Program
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
